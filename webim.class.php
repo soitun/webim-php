f61e1413460e88cb48aca19518194c02e07711b9
Hidden <zzdhidden@gmail.com>
2010-08-23 16:25:35 +0800

    Init

---
+++ webim.class.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * WebIM PHP Lib
+ *
+ * Author: Hidden <zzdhidden@gmail.com>
+ * Date: Mon Aug 23 15:15:41 CST 2010
+ *
+ */
+
+require_once('util.php');
+
+class WebIM
+{
+	/**
+	 * Online
+	 *
+	 * @param
+	 * @return
+	 *
+	 */
+	private $host = NULL;
+	private $port = NULL;
+	private $user = NULL;
+
+	public function __construct($host, $port, $user){
+	}
+	/**
+	 * Online
+	 *
+	 * @param
+	 * @return
+	 *
+	 */
+	public function online(){
+	}
+
+	public function check_connect(){
+	}
+}
+
+?>
