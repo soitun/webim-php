939439ef47e2d30b669017edb8f739cecc969589
Hidden <zzdhidden@gmail.com>
2010-08-30 17:08:30 +0800

    Downcase filename

--- util.php
+++ util.php
@@ -5,7 +5,7 @@
  *
  */
 if( !function_exists('json_encode') ) {
-	require_once 'JSON.php';
+	require_once 'json.php';
 	function json_encode($data) {
 		$json = new Services_JSON();
 		return( $json->encode($data) );
@@ -14,7 +14,7 @@ if( !function_exists('json_encode') ) {
 
 // Future-friendly json_decode
 if( !function_exists('json_decode') ) {
-	require_once 'JSON.php';
+	require_once 'json.php';
 	function json_decode($data) {
 		$json = new Services_JSON();
 		return( $json->decode($data) );
